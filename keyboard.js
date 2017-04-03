691915d37109a49a9230b22672e369a70fbea591
ElineaFD <auquiere.eleanore@gmail.com>
2017-04-03 11:02:49 +0200

    dans le js la selection des key done

--- keyboard.js
+++ keyboard.js
@@ -1,20 +1,33 @@
+window.addEventListener("DOMContentLoaded", keySelect());
 
-document.addEventListener('keydown', function(event) 
+
+function keySelect()
 {
-    if(event.key == "ArrowLeft"  || event.key == q) 
-    {
-        alert('Left was pressed');
-    }
-    else if(event.key == "ArrowUp" || event.key == z) 
-    {
-        alert('Up was pressed');
-    }
-    else if(event.key == "ArrowRight" || event.key == d) 
-    {
-        alert('Right was pressed');
-    }
-    else if(event.key == "ArrowDown" || event.key == s) 
-    {
-        alert('Down was pressed');
-    }
-});
+	document.addEventListener('keydown', function(event) 
+	{
+	    if(event.key == "ArrowLeft"  || event.key == "q") 
+	    {
+	        alert('Left was pressed');
+	    }
+	    else if(event.key == "ArrowUp" || event.key == "z") 
+	    {
+	        alert('Up was pressed');
+	    }
+	    else if(event.key == "ArrowRight" || event.key == "d") 
+	    {
+	        alert('Right was pressed');
+	    }
+	    else if(event.key == "ArrowDown" || event.key == "s") 
+	    {
+	        alert('Down was pressed');
+	    }
+	    else if(event.keyCode == 32 ) 
+	    {
+	        shootBubbles();
+	    }
+	});
+}
+
+function shootBubbles(){
+	return ;
+}
